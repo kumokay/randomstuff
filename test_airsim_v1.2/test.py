4d4c92d4205fd00e1e8773bc76a20347aed7e409
kumokay <kumokay@users.noreply.github.com>
2018-10-31 20:44:37 -0700

    Update test.py

--- test_airsim_v1.2/test.py
+++ test_airsim_v1.2/test.py
@@ -2,17 +2,17 @@ import msgpackrpc
 import base64
 import time
 
-for i in range(1, 5):
-    filepath = '/home/kumokay/github/placethings/config_line_backup/{}-0.png'.format(i)
+for i in range(25, 30):
+    filepath = 'D:\\airsim_v1.2.0\\screenshot\\Drone1\\{}-0.png'.format(i)
     with open(filepath, 'rb') as binary_file:
         # Read the whole file at once
         data = binary_file.read()
     data = base64.b64encode(data)
-
+    print(i)
     t1 = time.time()
-    client = msgpackrpc.Client(msgpackrpc.Address("172.17.49.51", 18800))
-    result = client.call('rec_img', data)
+    client = msgpackrpc.Client(msgpackrpc.Address("172.17.49.70", 18800))
+    result = client.call('push', data, time.time())
     t2 = time.time()
     print(result)
-    print(t2-t1)
+    # print(t2-t1)
     # time.sleep(3)
